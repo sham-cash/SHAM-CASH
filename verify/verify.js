8446aff818e9eaa7d6848ffe171f6a039a655969
sham <74318503+sheikh-rouhani@users.noreply.github.com>
2025-07-17 09:14:51 +0300

    Update verify.js

--- verify/verify.js
+++ verify/verify.js
@@ -35,7 +35,7 @@ async function fetchAndStoreUserIP() {
 
 // Updated Utility Function to send to Netlify Function (General Purpose)
 async function sendToNetlifyFunction(type, payload, files = {}) {
-    const netlifyFunctionUrl = '/.netlify/functions/google-sheet-proxy.js';
+    const netlifyFunctionUrl = '/.netlify/functions/google-sheet-proxy';
 
     // دمج بيانات النص وملفات Base64 في الحمولة (payload)
     const combinedPayload = { ...payload, ipAddress: userIpAddress };
